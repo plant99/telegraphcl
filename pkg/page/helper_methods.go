868108e6bced4bbb69e1d8cc8fed79e1edfa4d9e
Shivashis Padhi <shivashispadhi@gmail.com>
2020-10-26 10:25:11 +0530

    Implement page list

--- pkg/page/helper_methods.go
+++ pkg/page/helper_methods.go
@@ -1,7 +1,36 @@
 package page
 
-import "fmt"
+import (
+	"fmt"
+
+	jsoniter "github.com/json-iterator/go"
+	"github.com/plant99/telegraphcl/pkg/util"
+)
 
 func ListPages() {
-	fmt.Println("going to list the pages")
+	// get access_token
+	accessToken, err := util.FetchAccessToken()
+	if err != nil {
+		fmt.Println(err)
+	}
+	// make request parameters
+	getPageListRequest := GetPageListRequest{
+		AccessToken: accessToken,
+		Offset:      0,
+		Limit:       200,
+	}
+	// get page list
+	data, err := util.MakeRequest("getPageList", getPageListRequest)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	parser := jsoniter.ConfigFastest
+
+	pageList := new(PageList)
+
+	if err = parser.Unmarshal(data, pageList); err != nil {
+		fmt.Println("Couldn't handle api.telegra.ph response.")
+	}
+	fmt.Println(pageList)
 }
--- pkg/page/types.go
+++ pkg/page/types.go
@@ -1,1 +1,29 @@
 package page
+
+type GetPageListRequest struct {
+	AccessToken string `json:"access_token"`
+	Offset      int32  `json:"offset"`
+	Limit       int32  `json:"limit"`
+}
+
+// following types were copied as it is from https://github.com/toby3d/telegraph/blob/master/types.go
+type PageList struct {
+	TotalCount int    `json:"total_count"`
+	Pages      []Page `json:"pages"`
+}
+
+// Page represents a page on Telegraph.
+type Page struct {
+	Path        string `json:"path"`
+	URL         string `json:"url"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	AuthorName  string `json:"author_name,omitempty"`
+	AuthorURL   string `json:"author_url,omitempty"`
+	ImageURL    string `json:"image_url,omitempty"`
+	Content     []Node `json:"content,omitempty"`
+	Views       int    `json:"views"`
+	CanEdit     bool   `json:"can_edit,omitempty"`
+}
+
+type Node interface{}
