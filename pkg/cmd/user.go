827162cc1791ba1e6582c39e8a35dab78a49f4de
Shivashis Padhi <shivashispadhi@gmail.com>
2020-10-26 18:35:31 +0530

    Move creation of user to subcommand

--- pkg/cmd/user.go
+++ pkg/cmd/user.go
@@ -1,6 +1,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/plant99/telegraphcl/pkg/user"
 	"github.com/spf13/cobra"
 )
@@ -8,15 +10,36 @@ import (
 var userCmd = &cobra.Command{
 	Use:   "user",
 	Short: "Operations related to Telegraph account management",
+}
+
+var createCmd = &cobra.Command{
+	Use:   "create",
+	Short: "Create an user",
 	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Print("Enter your username: ")
+		var shortName string
+
+		// Taking input from user
+		fmt.Scanln(&shortName)
+		fmt.Print("Enter your fullname: ")
+		var fullName string
+		fmt.Scanln(&fullName)
+		fmt.Print("Enter an url to be displayed on your articles: ")
+		var authorUrl string
+		fmt.Scanln(&authorUrl)
 		userNew := user.User{
-			ShortName:   "shortname",
-			AuthorName:  "authorname",
-			AuthorUrl:   "https://shivashis.xyz",
+			ShortName:   shortName,
+			AuthorName:  fullName,
+			AuthorUrl:   authorUrl,
 			AccessToken: "",
 			AuthUrl:     "",
 			PageCount:   0,
 		}
+		fmt.Println(userNew)
 		user.CreateUser(userNew)
 	},
+}
+
+func init() {
+	userCmd.AddCommand(createCmd)
 }
