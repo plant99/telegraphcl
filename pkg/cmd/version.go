0cef2bda8d3526fc56287382f135e21ac7969635
Shivashis Padhi <shivashispadhi@gmail.com>
2020-11-07 20:22:51 +0530

    Store version in a string

--- pkg/cmd/version.go
+++ pkg/cmd/version.go
@@ -6,10 +6,16 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var version string
+
+func init() {
+	version = "v0.0.1-beta"
+}
+
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print the version number of telegraphcl",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("telegraphcl v0.0.1-beta")
+		fmt.Println("telegraphcl", version)
 	},
 }
